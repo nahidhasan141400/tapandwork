b9c060bdff64f96eb5e75b8f1449f686fdab6f4e
NahidPc <nahidhasan141400@gmail.com>
2022-05-16 20:51:21 +0600

    frist commit

---
+++ res/js/main.js
@@ -0,0 +1,12 @@
+let nav = document.querySelector('.nava');
+let show = document.querySelector(".show");
+
+show.addEventListener('click',(e)=>{
+    e.stopPropagation();
+    nav.className = 'nava';
+});
+
+nav.addEventListener('click',(e)=>{
+    e.stopPropagation();
+    nav.className = 'nava d';
+});
